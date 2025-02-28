9f9195f90b25db06c2638e343f372a18c8d27c27
faizshaikh17 <mohdfaiz8101@gmail.com>
2025-02-28 20:54:53 +0530

    categories, filtering/sorting func. added

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const productsGrid = document.getElementById('productsGrid');
+const searchBox = document.getElementById('searchBox');
+const categoryFilter = document.getElementById('categoryFilter');
+const sortOptions = document.getElementById('sortOptions');
+const cartIcon = document.getElementById('cartIcon');
+const cartCount = document.getElementById('cartCount');
+const cartModal = document.getElementById('cartModal');
+const closeModal = document.getElementById('closeModal');
+const cartContent = document.getElementById('cartContent');
 
-const products = [];
-const cart = [];
-const selectedVarieties = [];
+let products = [];
+let cart = [];
+let selectedVarieties = {};
 
 const API_URL = "http://localhost/products.php"
 
@@ -18,6 +26,7 @@ const loadProducts = async () => {
             selectedVarieties[product.id] = product.varieties[0].id
         })
 
+        allCategories();
 
     } catch (error) {
         console.error('Error loading products:', error);
@@ -25,5 +34,44 @@ const loadProducts = async () => {
     }
 }
 
+const allCategories = () => {
+    const categories = [...new Set(products.map(product => product.category))];
+    categories.forEach((category) => {
+        const option = document.createElement('option')
+        option.value = category;
+        option.textContent = category
+        categoryFilter.appendChild(option);
+    });
+}
+
+const allProducts = () => {
+    const searchValue = searchBox.value.toLowerCase();
+    const categoryValue = categoryFilter.value;
+
+    let filterProducts = products.filter((product) => {
+        const searchMatch = product.name.toLowerCase().includes(searchValue);
+        const categoryMatch = products.category === categoryValue
+        return searchMatch && categoryMatch
+    })
+
+    const sortValue = sortOptions.value;
+    filterProducts.sort((a, b) => {
+        const aVarietyId = selectedVarieties[a.id];
+        const bVarietyId = selectedVarieties[b.id];
+        const aVariety = a.varieties.find(variety => variety.id === aVarietyId);
+        const bVariety = b.varieties.find(variety => variety.id === bVarietyId);
+
+        let comparison = aVariety - bVariety;
+
+        if (sortValue === 'high-to-low') {
+            comparison = bVariety - aVariety;
+        }
+
+        return comparison;
+    });
+
+}
+
+loadProducts();
 
 
